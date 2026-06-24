4b282f2f88fac12ddfbee4aaddc0aae075e7705f
Hriday <hridayvermani12@gmail.com>
2026-06-24 19:34:37 -0400

    initialize main game file and create players

---
+++ Lab9_hridayv-cmd-1.py
@@ -0,0 +1,15 @@
+"""
+Program Name: Lab9_username-1.py
+Author: Hriday Vermani
+Purpose: Main game file that manages the game loop, coin matching rules, and user interaction.
+Starter Code: None 
+Date: June 24 2026
+"""
+from player import Player
+
+def main():
+    print("--- Coin Match Game ---")
+    
+    # Initialize the two players
+    player1 = Player("Player 1")
+    player2 = Player("Player 2")
