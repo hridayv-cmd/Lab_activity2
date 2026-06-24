ed0cf73b29588822a2c29a24cba30bf6da99672b
Hriday <hridayvermani12@gmail.com>
2026-06-24 19:07:47 -0400

    add toss and get_sideup methods to Coin class

--- coin.py
+++ coin.py
@@ -12,3 +12,13 @@ class Coin:
         """Initializes the coin and determines its initial side up."""
         self.__sideup = 'Heads'
         self.toss()  # Automatically toss to randomize the initial state
+    def toss(self):
+        """Generates a random choice and sets __sideup to 'Heads' or 'Tails'."""
+        if random.randint(0, 1) == 0:
+            self.__sideup = 'Heads'
+        else:
+            self.__sideup = 'Tails'
+
+    def get_sideup(self):
+        """Returns the current value of __sideup."""
+        return self.__sideup
