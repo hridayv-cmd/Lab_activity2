9dd476a7800aec31b9b02671380b9f5cd09c2503
Hriday <hridayvermani12@gmail.com>
2026-06-24 20:07:58 -0400

    add match detection logic and wallet updates

--- Lab9_hridayv-cmd-1.py
+++ Lab9_hridayv-cmd-1.py
@@ -13,7 +13,7 @@ def main():
     # Initialize the two players
     player1 = Player("Player 1")
     player2 = Player("Player 2")
-    
+
     # Show initial balances
     print(f"{player1.get_name()} has {player1.get_wallet()} coins.")
     print(f"{player2.get_name()} has {player2.get_wallet()} coins.\n")
@@ -27,3 +27,23 @@ def main():
         # Force players to toss their respective coins
         player1.toss_coin()
         player2.toss_coin()
+        # Get the results of the toss
+        side1 = player1.get_coin_side()
+        side2 = player2.get_coin_side()
+        
+        print(f"{player1.get_name()} tossed {side1}")
+        print(f"{player2.get_name()} tossed {side2}")
+        
+        # Game Rules Logic
+        if side1 == side2:
+            player1.win_coin()
+            player2.lose_coin()
+            print(f"...It's a Match! {player1.get_name()} wins a coin.\n")
+        else:
+            player2.win_coin()
+            player1.lose_coin()
+            print(f"...No Match! {player2.get_name()} wins a coin.\n")
+            
+        # Report current scores
+        print(f"{player1.get_name()} has {player1.get_wallet()} coins.")
+        print(f"{player2.get_name()} has {player2.get_wallet()} coins.\n")
